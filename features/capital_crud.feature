b709a528e3b8505857201118063cafeb36f29288
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-06-20 17:37:43 -0300

    first cucumber test for capital - empty list

---
+++ features/capital_crud.feature
@@ -0,0 +1,14 @@
+# encoding: utf-8
+# language: pt
+
+  Funcionalidade: Cadastro
+    Como usuario da Aplicacao
+    Quero poder cadastrar Capitals na Aplicacao
+    Para garantir que tenhamos uma Aplicacao com cadastro completo
+
+  Cenario: Listagem Vazia
+    Dado que nao exista nenhum capital
+    Quando acesso a listagem de capital
+    Entao devo estar na listagem de capital
+    E a pagina tem as colunas "ID|Nome|Ações"
+    E a pagina nao tem resultados "Nenhum Resultado Encontrado!"
---
+++ features/step_definitions/capital_steps.rb
@@ -0,0 +1,20 @@
+Dado(/^que nao exista nenhum capital$/) do
+  Capital.all { |m| m.delete }
+end
+
+Quando(/^acesso a listagem de capital$/) do
+  @page = CapitalIndex.new
+  @page.load
+end
+
+Entao(/^devo estar na listagem de capital$/) do
+  expect(CapitalIndex.new.displayed?).to be true
+end
+
+Entao(/^a pagina tem as colunas "(.*?)"$/) do |columns|
+  expect(@page.table_headers.map { |h| h.text }).to eq(columns.split('|'))
+end
+
+Entao(/^a pagina nao tem resultados "(.*?)"$/) do |msg|
+  expect(@page.no_results.text).to eq(msg)
+end
