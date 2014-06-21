7ad5e39326d56274f3fd2d9ed64175764cb9ef82
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-06-21 19:59:31 -0300

    join the steps into one file

--- features/step_definitions/healthcheck_steps.rb
+++ features/step_definitions/healthcheck_steps.rb
@@ -2,3 +2,11 @@ Quando(/^eu chamo o servico de healthcheck$/) do
   @service = Service::Healthcheck.new
   @service.healthcheck
 end
+
+Entao(/^o response deve conter "(.*?)"$/) do |msg|
+  expect(@service.response).to include(msg)
+end
+
+Entao(/^o response deve ter http code (\d+)$/) do |code|
+  expect(@service.code).to eq(code.to_i)
+end
