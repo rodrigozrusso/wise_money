d3dfccf18cb8c855157104175bf8feea3c867f96
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-06-21 20:03:26 -0300

    remove unnecessary css classes

--- features/pages/delete_modal_section.rb
+++ features/pages/delete_modal_section.rb
@@ -3,8 +3,6 @@ module Pages
 
     element :title, '.delete-modal-title'
     element :body, '.delete-modal-body'
-    element :cancel_button, '.delete-modal-cancel'
-    element :confirm_button, '.delete-modal-confirm'
 
   end
 end
