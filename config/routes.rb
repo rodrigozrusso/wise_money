0527a384e9ce72b08fe48cd311b41554b3f31f7c
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-06-22 22:41:07 -0300

    change the capital routes to keep the user in the new capital page when validation and also in the edit capital page when validation occurs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   resources :healthchecks, only: [:index]
   resources :transactions
-  resources :capitals
+
+  resources :capitals, except: [:create, :update]
+  post 'capitals/new', to: 'capitals#create', as: :create_capital
+  put 'capitals/:id/edit', to: 'capitals#update'
+  patch 'capitals/:id/edit', to: 'capitals#update', as: :update_capital
 
   root 'capitals#index'
 
