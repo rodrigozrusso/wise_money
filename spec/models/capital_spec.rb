a4c0304a39476c3474d39a0603e21b5a1a6a83a9
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-06-29 17:10:07 -0300

    adding specs to capital model to test the validations

--- spec/models/capital_spec.rb
+++ spec/models/capital_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe Capital do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+
+    context 'valid' do
+      let(:capital) { Fabricate.build(:capital_valid) }
+      before {capital.save}
+
+      it { expect(capital).to be_valid }
+      it { expect(capital.errors.map{|k,v| {k => v}}).to eq([]) }
+    end
+
+    context 'not null' do
+      let(:capital) { Fabricate.build(:capital) }
+      before {capital.save}
+
+      it { expect(capital).to_not be_valid }
+      it { expect(capital.errors.map{|k,v| {k => v}}).to eq([{name: "can't be blank"}]) }
+    end
+
+    context 'max lenght' do
+      let(:capital) { Fabricate.build(:capital_maximum_lenght) }
+      before {capital.save}
+
+      it { expect(capital).to_not be_valid }
+      it { expect(capital.errors.map{|k,v| {k => v}}).to eq([{name: "is too long (maximum is 150 characters)"}]) }
+    end
+
+    context 'not unique' do
+      let(:attributes) { Fabricate.attributes_for(:capital_valid) }
+      before { Capital.create(attributes) }
+      let(:capital) { Capital.create(attributes) }
+
+      it { expect(capital).to_not be_valid }
+      it { expect(capital.errors.map{|k,v| {k => v}}).to eq([{name: "has already been taken"}]) }
+    end
+
+  end
+
 end
