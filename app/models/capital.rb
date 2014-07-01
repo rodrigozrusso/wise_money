8270985c8ff861c8dc952ed583d1b552d0de7cb0
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-07-01 20:51:46 -0300

    implement to_s in models

--- app/models/capital.rb
+++ app/models/capital.rb
@@ -2,4 +2,8 @@ class Capital < ActiveRecord::Base
 
   validates :name, presence: true, length: {maximum: 150}, uniqueness: { case_sensitive: false }
 
+  def to_s
+    "#{id} - #{name}"
+  end
+
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,8 @@ class Transaction < ActiveRecord::Base
 
   belongs_to :income_capital, class_name: 'Capital'
   belongs_to :expense_capital, class_name: 'Capital'
+  def to_s
+    "#{id} - #{description} - #{status_humanize} - #{total} - #{checked_at} - #{income_capital} - #{expense_capital}"
+  end
+
 end
