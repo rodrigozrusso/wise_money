2a663a768835f0f88f131465baa9d2c13692ffbd
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-07-02 20:46:56 -0300

    improve the fabricators

--- spec/fabricators/capitals.rb
+++ spec/fabricators/capitals.rb
@@ -8,6 +8,9 @@ end
 Fabricator(:capital_saving_account, from: :capital_valid) do
   name 'saving account'
 end
+Fabricator(:capital_appartment, from: :capital_valid) do
+  name 'appartment'
+end
 
 Fabricator(:capital_maximum_lenght, from: :capital_valid) do
   name 'a' * 151
--- spec/fabricators/transactions.rb
+++ spec/fabricators/transactions.rb
@@ -6,8 +6,16 @@ Fabricator(:transaction_valid, from: :transaction) do
   status { TransactionStatus.list.sample }
   total { Forgery(:monetary).money(min: -100.0, max: 1000.0) }
   checked_at { Forgery(:basic).number(at_least: 1, at_most: 30).days.ago }
-  income_capital { Capital.all.sample }
-  expense_capital { Capital.all.sample }
+end
+
+Fabricator(:transaction_with_income, from: :transaction_valid) do
+  income_capital { Fabricate(:capital_valid) }
+end
+Fabricator(:transaction_with_expense, from: :transaction_valid) do
+  expense_capital { Fabricate(:capital_valid) }
+end
+Fabricator(:transaction_with_income_and_expense, from: :transaction_with_income) do
+  expense_capital { Fabricate(:capital_valid) }
 end
 
 Fabricator(:transaction_light_bill, from: :transaction_valid) do
@@ -15,7 +23,7 @@ Fabricator(:transaction_light_bill, from: :transaction_valid) do
   status { TransactionStatus.list[0] }
   total 100.99
   checked_at Date.new(2014,6,23)
-  income_capital { Fabricate.build(:capital_saving_account) }
+  income_capital { Fabricate(:capital_saving_account) }
 end
 
 Fabricator(:transaction_maximum_lenght, from: :transaction_valid) do
